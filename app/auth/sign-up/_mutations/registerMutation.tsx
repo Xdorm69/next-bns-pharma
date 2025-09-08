1d7a94fd691405744b10e08c4ae8a72ae2eedbc4
Amitoj <amitoj.mehta@gmail.com>
2025-09-08 21:34:24 +0530

    lint fix

--- app/auth/sign-up/_mutations/registerMutation.tsx
+++ app/auth/sign-up/_mutations/registerMutation.tsx
@@ -3,7 +3,13 @@
 import { useMutation } from "@tanstack/react-query";
 import { toast } from "sonner";
 import { fetchApi } from "@/Hooks/api";
-import { User } from "@prisma/client";
+
+//HOW resultant data will come from api
+export type UserResponse = {
+  id: string;
+  name: string;
+  email: string;
+};
 
 type SignupDataType = {
   name: string;
@@ -14,13 +20,13 @@ type SignupDataType = {
 export function useRegisterMutation() {
   return useMutation({
     mutationKey: ["register-user"],
-    mutationFn: async (formData: SignupDataType): Promise<User> => {
-      const res = await fetchApi<User>("/api/auth/register", {
+    mutationFn: async (formData: SignupDataType): Promise<UserResponse> => {
+      const res = await fetchApi<UserResponse>("/api/auth/register", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(formData),
       });
-      return res.data as User;
+      return res.data as UserResponse;
     },
     onMutate: () => {
       toast.loading("Registering user...", { id: "user" });
@@ -28,7 +34,7 @@ export function useRegisterMutation() {
     onSuccess: (data) => {
       toast.success(`Welcome, ${data.name}!`, { id: "user" });
     },
-    onError: (error: unknown) => {
+    onError: (error: {message?: string}) => {
       toast.error(error.message || "User registration failed", { id: "user" });
     },
   });
