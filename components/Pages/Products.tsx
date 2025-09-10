977483500bcb548f0e1df7bd7c6102873a910eaa
Amitoj <amitoj.mehta@gmail.com>
2025-09-10 19:27:26 +0530

    added filters

--- components/Pages/Products.tsx
+++ components/Pages/Products.tsx
@@ -1,19 +1,34 @@
+import Link from "next/link";
 import HomePageProductRender from "../HomePageProductRender";
+import { Button } from "../ui/button";
 
 export default function ProductsPage() {
   return (
     <section className="min-h-screen w-full">
       <div className="cont">
         <div className="mb-4">
-          <h1 className="text-4xl font-bold mb-4 font-mono">PCD Popular Products</h1>
+          <div className="flex items-center justify-between">
+            <h1 className="text-4xl font-bold mb-4 font-mono">
+              PCD Popular Products
+            </h1>
+            <Link href={"/products/pcd"}>
+              <Button variant="outline">View More</Button>
+            </Link>
+          </div>
           <HomePageProductRender type="pcd" />
         </div>
         <div className="mt-12">
-          <h1 className="text-4xl font-bold mb-4 font-mono">Third Party Popular Products</h1>
+          <div className="flex items-center justify-between">
+            <h1 className="text-4xl font-bold mb-4 font-mono">
+            Third Party Popular Products
+          </h1>
+          <Link href={"/products/third-party"}>
+            <Button variant="outline">View More</Button>
+          </Link>
+          </div>
           <HomePageProductRender type="third-party" />
         </div>
       </div>
     </section>
   );
 }
-
