2179a52c593f77adfbdbaa1667148227df481878
amitoj <amitoj.mehta@gmail.com>
2026-04-13 20:34:51 +0530

    container fix on products/[id]

--- app/products/[id]/loading.tsx
+++ app/products/[id]/loading.tsx
@@ -2,7 +2,7 @@ import Skeleton from "@/components/ui/skeleton";
 
 export default function Load() {
   return (
-    <div className="cont py-24">
+    <div className="container py-24">
       <Skeleton className="h-8 w-3/4 mb-4" />
       <Skeleton className="h-64 w-full mb-4" />
       <Skeleton className="h-6 w-1/2" />
